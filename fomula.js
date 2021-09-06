97ab3d9955c35bdf47863c3535917c35ad504177
Dmitriy Roi <dimarionss@gmail.com>
2021-09-06 09:45:45 +0300

    add function

--- fomula.js
+++ fomula.js
@@ -1,17 +1,22 @@
 
-var N = 0;
-while (N <= 5000) {
-  N += 1;
-  var a = 1n,
-    b = 1n
-  for (i = 3; i <= N; i++) {
-    let sum = a + b;
-    a = b;
-    b = sum;
-  }
-  if(b.toString().length == 1000){
-    console.log('F', N, '<==========NUMBER IS============>', b)
-    alert('Answer: '+'F'+ N)
-    break;
+
+function fib (count){
+  var N = 0;
+  while (N <= count) {
+    N += 1;
+    var a = 1n,
+      b = 1n
+    for (i = 3; i <= N; i++) {
+      let sum = a + b;
+      a = b;
+      b = sum;
+    }
+    if(b.toString().length == 1000){
+      console.log('F', N, '<==========NUMBER IS============>', b)
+      alert('Answer: '+'F'+ N)
+      break;
+    }
   }
 }
+
+fib(5000);
