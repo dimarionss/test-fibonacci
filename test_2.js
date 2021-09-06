696a547c9883d3b6920fc0d4c2b8d46688a157c4
Dmitriy Roi <dimarionss@gmail.com>
2021-09-06 12:29:20 +0300

    add test 2

--- test_2.js
+++ test_2.js
@@ -11,18 +11,4 @@ function splitN(n) {
   }
   alert("How many ways exist to reach a sum of 100 using 2 or more positive integers?" + " " + "Answer: " + nbr[nbr.length - 1])
 }
-
-
-// function waysFounder(val) {
-
-//   let ways = new Array(val + 1).fill(0);
-//       ways[0] = 1;
-//   for (let i = 1; i <= val - 1; i++) {
-//       for (let j = i; j <= val; j++) {
-//           ways[j] += ways[j - i];
-//       }
-//   }
-//   return ways[ways.length - 1]
-// }
-
 splitN(100)
